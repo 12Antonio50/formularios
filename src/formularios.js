a35d6203d83a1d008f7b82f6b7d239196cb951cb
12Antonio50 <ja.mojica.a@edu.utc.mx>
2023-03-04 14:15:02 -0600

    res

--- src/formularios.js
+++ src/formularios.js
@@ -12,89 +12,91 @@ import './css/style.css';
 import logo from './img/utc.png';
 import { Button } from '@mui/material';
 
-function App() {
-  const ocultar = () => {
-    document.getElementById('opcion1').style.display = 'none';
-};
-  return (
-    <>
-      <div className="magin-up" id="opcion1">
-      <div className="text-1"
-                style={{
-                    left: '50%',
-                    top: '8%',
-                    transform: 'translate(-50%, -50%)',
-                    textAlign: "center",
-                    width: 'auto',
-                    height: '61px',
-                    position: 'absolute'
-                }}>
-                  FORMULARIO DE EVALUACION DE LOS DOCENTES
-            </div>
-        <div className="box-white">
-          
-          <img className="logo-utc" src={logo} alt='for sell'></img>
-        </div>
-
-        <div className="page-two">
-          <div className="box">
-            <div className="office">
-              <p>Iniciar sesión con Office 365</p>
-              <button className="button-365" >Iniciar sesión</button>
-            </div>
-
+class App extends React.Component {
+  render() {
+    const ocultar = () => {
+      document.getElementById('opcion1').style.display = 'none';
+    };
+    return (
+      <>
+        <div className="magin-up" id="opcion1">
+          <div className="text-1"
+            style={{
+              left: '50%',
+              top: '8%',
+              transform: 'translate(-50%, -50%)',
+              textAlign: "center",
+              width: 'auto',
+              height: '61px',
+              position: 'absolute'
+            }}>
+            FORMULARIO DE EVALUACION DE LOS DOCENTES
           </div>
-          <Button
-            style={{
-              textAlign: "center",
-              position: 'absolute',
-              left: '43%',
-              top: '80%',
-              transform: 'translate(-50%, -50%)',
-              width: '150px',
-              height: '40px',
-              backgroundColor: '#0B1A61'
-            }}
-            variant="contained"
-            href="/alumno"
-          >
-            Alumno
-          </Button>
+          <div className="box-white">
 
-          <Button
-          onClick={ocultar}
-            style={{
-              textAlign: "center",
-              position: 'absolute',
-              left: '57%',
-              top: '80%',
-              transform: 'translate(-50%, -50%)',
-              width: '150px',
-              height: '40px',
-              backgroundColor: '#0B1A61'
-            }}
-            variant="contained"
-            href="/BasePag"
-          >
-            Inicio
-          </Button>
+            <img className="logo-utc" src={logo} alt='for sell'></img>
+          </div>
+
+          <div className="page-two">
+            <div className="box">
+              <div className="office">
+                <p>Iniciar sesión con Office 365</p>
+                <button className="button-365" >Iniciar sesión</button>
+              </div>
+
+            </div>
+            <Button
+              style={{
+                textAlign: "center",
+                position: 'absolute',
+                left: '43%',
+                top: '80%',
+                transform: 'translate(-50%, -50%)',
+                width: '150px',
+                height: '40px',
+                backgroundColor: '#0B1A61'
+              }}
+              variant="contained"
+              href="/alumno"
+            >
+              Alumno
+            </Button>
+
+            <Button
+              onClick={ocultar}
+              style={{
+                textAlign: "center",
+                position: 'absolute',
+                left: '57%',
+                top: '80%',
+                transform: 'translate(-50%, -50%)',
+                width: '150px',
+                height: '40px',
+                backgroundColor: '#0B1A61'
+              }}
+              variant="contained"
+              href="/BasePag"
+            >
+              Inicio
+            </Button>
+          </div>
+          <div className="magin-donw"></div>
         </div>
-        <div className="magin-donw"></div>
-      </div>
 
 
-      <Routes>
-        <Route exact path="/BasePag" element={<BasePag />} />
-        <Route exact path="/FormulariosFi" element={<Formulariosfinalizados />} />
-        <Route exact path="/VisualizarR" element={<VisualizarRespuestas />} />
-        <Route exact path="/FormAct" element={<FormularioActivo />} />
-        <Route exact path="/Eliminar" element={<EliminarFormulario />} />
-        <Route exact path="/Crear" element={<Crear />} />
-        <Route exact path="/borrador" element={<Borrador />} />
-        <Route exact path="/alumno" element={<Alumno />} />
-      </Routes>
+        <Routes>
+          <Route exact path="/BasePag" element={<BasePag />} />
+          <Route exact path="/FormulariosFi" element={<Formulariosfinalizados />} />
+          <Route exact path="/VisualizarR" element={<VisualizarRespuestas />} />
+          <Route exact path="/FormAct" element={<FormularioActivo />} />
+          <Route exact path="/Eliminar" element={<EliminarFormulario />} />
+          <Route exact path="/Crear" element={<Crear />} />
+          <Route exact path="/borrador" element={<Borrador />} />
+          <Route exact path="/alumno" element={<Alumno />} />
+        </Routes>
 
-    </>
-  );
+      </>
+    );
+  }
 }
 export default App;
