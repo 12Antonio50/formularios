2fba10ae3c63f2670c078a6f6c485db6f93344e5
12Antonio50 <ja.mojica.a@edu.utc.mx>
2023-03-11 15:55:09 -0600

    intento

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import "./css/style.css";
-import Profile from "./Profile";
-import { HashRouter} from "react-router-dom";
+
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import BasePag from './componentes/BasePag';
+import Formulariosfinalizados from './componentes/FormulariosF';
+import VisualizarRespuestas from './componentes/VisualizarRes';
+import FormularioActivo from './componentes/FormActiv';
+import EliminarFormulario from './componentes/EliminarForm';
+import Borrador from './componentes/Borradores';
+import Crear from './componentes/Crear';
+import Alumno from './componentes/Alumno';
+import Login from './componentes/Login';
+
 
 export default function App() {
 
-    return (
-      <div className="App">
-      <HashRouter>
-        <Profile />
-      </HashRouter>
-    </div>
-    );
-  }
 
+        return(
+          <>
+            <div className='App'>  
+          <Routes>
+          <Route path="/" element={<Login/>}/>
+            <Route path="/basepag" element={<BasePag/>}/>
+            <Route path="/formulariosfi" element={<Formulariosfinalizados/>}/>
+            <Route path="/visualizarr" element={<VisualizarRespuestas/>}/>
+            <Route path="/formact" element={<FormularioActivo/>}/>
+            <Route path="/eliminar" element={<EliminarFormulario/>}/>
+            <Route path="/crear" element={<Crear/>}/>
+            <Route path="/borrador" element={<Borrador/>}/>
+            <Route path="/alumno" element={<Alumno/>}/>
+          </Routes>
+          </div>    
+        </>
+  );
+  }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import BasePag from './componentes/BasePag';
+import Formulariosfinalizados from './componentes/FormulariosF';
+import VisualizarRespuestas from './componentes/VisualizarRes';
+import FormularioActivo from './componentes/FormActiv';
+import EliminarFormulario from './componentes/EliminarForm';
+import Borrador from './componentes/Borradores';
+import Crear from './componentes/Crear';
+import Alumno from './componentes/Alumno';
+import Login from './componentes/Login';
 
 
 const rootElement = document.getElementById("root");
@@ -10,7 +19,22 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    <div className='App'>  
+          <Routes>
+          <Route path="/" element={<Login/>}/>
+            <Route path="/basepag" element={<BasePag/>}/>
+            <Route path="/formulariosfi" element={<Formulariosfinalizados/>}/>
+            <Route path="/visualizarr" element={<VisualizarRespuestas/>}/>
+            <Route path="/formact" element={<FormularioActivo/>}/>
+            <Route path="/eliminar" element={<EliminarFormulario/>}/>
+            <Route path="/crear" element={<Crear/>}/>
+            <Route path="/borrador" element={<Borrador/>}/>
+            <Route path="/alumno" element={<Alumno/>}/>
+          </Routes>
+          </div>    
+    </BrowserRouter>
   </StrictMode>
 );
 
